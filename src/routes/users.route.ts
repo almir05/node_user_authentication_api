cf74d4dca24f8b8fd54bb8465dc4604f7ef68d87
almir05 <almir.marcelo08@gmail.com>
2021-12-07 13:05:48 -0400

    Facilitando o uso de StatusCode

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
@@ -6,12 +7,12 @@ usersRoute.get('/users', (req:Request, res:Response, next:NextFunction) => {
   const users = [{
     userName: 'Almir'
   }];
-  res.status(200).send(users);
+  res.status(StatusCodes.OK).send(users);
 });
 
 usersRoute.get('/users/:uuid', (req:Request<{uuid: string}>, res:Response, next:NextFunction) => {
   const uuid = req.params.uuid;
-  res.status(200).send({ uuid });
+  res.status(StatusCodes.OK).send({ uuid });
 });
 
 export default usersRoute;
