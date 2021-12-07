770d0bd19f828603c9464f27478694fb4e2cf641
almir05 <almir.marcelo08@gmail.com>
2021-12-07 12:57:26 -0400

    first commit

---
+++ src/routes/users.route.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response, Router } from "express";
+
+const usersRoute = Router();
+
+usersRoute.get('/users', (req:Request, res:Response, next:NextFunction) => {
+  const users = [{
+    userName: 'Almir'
+  }];
+  res.status(200).send(users);
+});
+
+usersRoute.get('/users/:uuid', (req:Request<{uuid: string}>, res:Response, next:NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(200).send({ uuid });
+});
+
+export default usersRoute;
